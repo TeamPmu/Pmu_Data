3750119fcf60f05bc0bac79ff28e0031e70c1f42
mino1998 <mhlee5894@gmail.com>
2023-10-17 13:33:49 +0900

    plz

--- lambda_emotion/emotionclassifier.py
+++ lambda_emotion/emotionclassifier.py
@@ -7,22 +7,42 @@ from tensorflow.keras.models import load_model
 import keras.backend as K
 import requests
 import io
-from facenet_pytorch import MTCNN
+import boto3
 
+# Initialize the S3 client
+s3 = boto3.client('s3')
+
+# Define S3 bucket name and the file paths within the bucket
+s3_bucket = 'your-s3-bucket'
+model_s3_file_path = 'model_1/'
+mtcnn_s3_file_path = 'facenet_pytorch/'
+
+# Specify the local paths where you want to save the downloaded files
+local_model_path = '/tmp/model_1/'
+local_mtcnn_path = '/tmp/facenet_pytorch/'
+
+# Download the model files from S3 (assuming the model consists of multiple files)
+for file_name in os.listdir(model_s3_file_path):
+    s3.download_file(s3_bucket, model_s3_file_path + file_name, local_model_path + file_name)
+
+# Download the MTCNN files from S3
+for file_name in os.listdir(mtcnn_s3_file_path):
+    s3.download_file(s3_bucket, mtcnn_s3_file_path + file_name, local_mtcnn_path + file_name)
+
+# Load the model as a global variable
+model = load_model(local_model_path)
+
+# Load the MTCNN module as a global variable
+mtcnn = MTCNN(select_largest=False, post_process=False)
 
 class ImageClassifier:
 
-    def __init__(self, model_dir: str,
-                 ) -> None:
-        self.model = load_model(model_dir,
-                                custom_objects={'get_f1': self.get_f1})
+    def __init__(self, model, mtcnn):
+        self.model = model
+        self.mtcnn = mtcnn
         self.url = ""
-
         self.image_size = {'IMG_HEIGHT': 128, 'IMG_WIDTH': 128}
-
         self.labels = ['neutral', 'sad', 'happy', 'angry']
-
-        self.mtcnn = MTCNN(select_largest=False, post_process=False)
 
     def get_f1(y_true, y_pred):
 
@@ -80,15 +100,13 @@ class ImageClassifier:
         return pred
 
 
-imageclassifier = ImageClassifier(
-    model_dir="/var/task/model_1",
-)
-
-
 def lambda_handler(event, context=None, url=""):
     '''
-    lambda_handler arugment -> event : json
+    lambda_handler argument -> event: json
     '''
+    global model, mtcnn
+
+    imageclassifier = ImageClassifier(model, mtcnn)
     pred = imageclassifier.run(event['url'])
 
     return {
